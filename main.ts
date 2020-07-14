49d017ba8cafe94b952fc3a2e0adc99944b61a5f
Anparra <47276601+Anparra@users.noreply.github.com>
2020-07-14 09:22:20 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 namespace SpriteKind {
     export const fridge = SpriteKind.create()
 }
-function placeFridge () {
+function placeFridgeZone3 () {
     fridge1 = sprites.create(img`
         . . d d d d d d d d d d d d d d
         . . d 1 1 1 1 1 1 1 1 1 1 1 1 1
@@ -79,13 +79,177 @@ function placeFridge () {
     `, SpriteKind.fridge)
     tiles.placeOnTile(fridge4, tiles.getTileLocation(20, 1))
 }
+function placeFridgeZone2() {
+    fridge1 = sprites.create(img`
+        . . d d d d d d d d d d d d d d
+        . . d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d d d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 b b b b b b b b b b b b b b
+        d b b b b b b b b b b b b b b b
+        d b d d d d d d d d d d d d d d
+    `, SpriteKind.fridge)
+    tiles.placeOnTile(fridge1, tiles.getTileLocation(16, 0))
+    fridge2 = sprites.create(img`
+        d d d d d d d d d d d d d d . .
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d . .
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d d d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        b b b b b b b b b b b b b b 1 d
+        b b b b b b b b b b b b b b b d
+        d d d d d d d d d d d d d d b d
+    `, SpriteKind.fridge)
+    tiles.placeOnTile(fridge2, tiles.getTileLocation(17, 0))
+    fridge3 = sprites.create(img`
+        d b d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d b d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b b b b b b b b b b b b b b b
+        f b d d d d d d d d d d d d d d
+        f b d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b b b b b b b b b b b b b b b
+        f b f f f f f f f f f f f f f f
+        f f f f f f f f f f f f f f f f
+    `, SpriteKind.fridge)
+    tiles.placeOnTile(fridge3, tiles.getTileLocation(16, 1))
+    fridge4 = sprites.create(img`
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        b b b b b b b b b b b b b b b f
+        d d d d d d d d d d d d d d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        b b b b b b b b b b b b b b b f
+        f f f f f f f f f f f f f f b f
+        f f f f f f f f f f f f f f f f
+    `, SpriteKind.fridge)
+    tiles.placeOnTile(fridge4, tiles.getTileLocation(17, 1))
+}
+function placeFridgeZone1() {
+    fridge1 = sprites.create(img`
+        . . d d d d d d d d d d d d d d
+        . . d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d d d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d 1 b b b b b b b b b b b b b b
+        d b b b b b b b b b b b b b b b
+        d b d d d d d d d d d d d d d d
+    `, SpriteKind.fridge)
+    tiles.placeOnTile(fridge1, tiles.getTileLocation(13, 0))
+    fridge2 = sprites.create(img`
+        d d d d d d d d d d d d d d . .
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d . .
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d d d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 d
+        b b b b b b b b b b b b b b 1 d
+        b b b b b b b b b b b b b b b d
+        d d d d d d d d d d d d d d b d
+    `, SpriteKind.fridge)
+    tiles.placeOnTile(fridge2, tiles.getTileLocation(14, 0))
+    fridge3 = sprites.create(img`
+        d b d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        d b d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b b b b b b b b b b b b b b b
+        f b d d d d d d d d d d d d d d
+        f b d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b d 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b f 1 1 1 1 1 1 1 1 1 1 1 1 1
+        f b b b b b b b b b b b b b b b
+        f b f f f f f f f f f f f f f f
+        f f f f f f f f f f f f f f f f
+    `, SpriteKind.fridge)
+    tiles.placeOnTile(fridge3, tiles.getTileLocation(13, 1))
+    fridge4 = sprites.create(img`
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b d
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        b b b b b b b b b b b b b b b f
+        d d d d d d d d d d d d d d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        1 1 1 1 1 1 1 1 1 1 1 1 1 d b f
+        b b b b b b b b b b b b b b b f
+        f f f f f f f f f f f f f f b f
+        f f f f f f f f f f f f f f f f
+    `, SpriteKind.fridge)
+    tiles.placeOnTile(fridge4, tiles.getTileLocation(14, 1))
+}
 let fridge4: Sprite = null
 let fridge3: Sprite = null
 let fridge2: Sprite = null
 let fridge1: Sprite = null
 namespace fridge {
     //% block
-    export function zone3_fridge(){
-        placeFridge()
+    export function zone1_fridge(){
+        placeFridgeZone1()
+    }
+    //% block
+    export function zone2_fridge() {
+        placeFridgeZone2()
+    }
+    //% block
+    export function zone3_fridge() {
+        placeFridgeZone3()
     }
 }
